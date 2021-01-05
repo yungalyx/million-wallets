8194b4cee07cbc0a28d71479b083c2e11048bfd4
yungalyx <52767711+yungalyx@users.noreply.github.com>
2021-01-04 19:28:15 -0800

    fix

--- src/components/balance.js
+++ src/components/balance.js
@@ -21,11 +21,11 @@ export default function Balance(props){
     const [divBy0, setdivby0] = useState(false)
 
     useEffect(() => {
-        if(props.item.quote_rate === 0) {
+        if(props?.item?.quote_rate === 0) {
             setdivby0(true)
         }
         // console.log(process.env.REACT_APP_COVALENT_KEY)
-    }, []);
+    }, [props.item.quote_rate]);
 
 
     return(
