99645b89ca5337f1220c79d6f635af92cbbda661
yungalyx <52767711+yungalyx@users.noreply.github.com>
2021-01-06 22:24:02 -0800

    netlify mad

--- src/components/transfer.js
+++ src/components/transfer.js
@@ -19,7 +19,7 @@ export default function Transfer(props){
         <tr>  
             <Tdn>{props.item.block_signed_at} </Tdn>
             <Tdn>{props.item.transfers[0]?.from_address} </Tdn>
-            <Tdn> <a href={"https://etherscan.io/tx/"+props.item.transfers[0]?.tx_hash} target="_blank">{props.item.transfers[0]?.tx_hash} </a> </Tdn>
+            <Tdn> <a href={"https://etherscan.io/tx/"+props.item.transfers[0]?.tx_hash} target="_blank" rel="noreferrer">{props.item.transfers[0]?.tx_hash} </a> </Tdn>
             <Tdn>{props.item.transfers[0]?.to_address} </Tdn>
             <Td credit={credit}>{props.item.transfers[0].delta_quote!=null ? 
             "$" + props.item.transfers[0]?.delta_quote.toFixed(2) + " USD" : 
